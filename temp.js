55be40ae5a2f0f647f40e6d7233288a2967dab06
Yonela <yonelamnagele1700@gmail.com>
2024-05-20 12:45:57 +0200

    added files

---
+++ temp.js
@@ -0,0 +1,15 @@
+let celsius = document.querySelector("#celsius")
+let fahrenheit = document.querySelector("#fahrenheit")
+
+window.addEventListener("load", () => celsius.focus());
+
+celsius.addEventListener("input", () => {
+    fahrenheit.value = ((celsius.value * 9) / 5 + 32).toFixed(2);
+    if(!celsius.value) fahrenheit.value = "";
+});
+
+fahrenheit.addEventListener("input", () => {
+    celsius.value = (((fahrenheit.value - 32) * 5) / 9).toFixed(2);
+    if(!fahrenheit.value) celsius.value = "";
+})
+
